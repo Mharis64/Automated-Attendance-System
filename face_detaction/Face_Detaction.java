3d07771cda4d436a5463052e594e5f8bac6f6356
MuhammadMuzammilkhan <115549730+MuhammadMuzammilkhan@users.noreply.github.com>
2022-11-04 03:04:10 -0700

    Add files via upload

---
+++ face_detaction/Face_Detaction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package face_detaction;
+
+/**
+ *
+ * @author EliteBook
+ */
+public class Face_Detaction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Record r = new Record();
+        r.setVisible(true);
+        Record_Principal p = new Record_Principal();
+        p.setVisible(true);
+    }
+    
+}
